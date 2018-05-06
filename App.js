fda51bb5b83955a477047a1bd9178f9999ccf34c
guiyongbin <739952043@qq.com>
2018-05-06 16:31:05 +0800

    testShop2

---
+++ App.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import Counter from "./src/component/Counter"
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+type Props = {};
+export default class App extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.initValues = [1, 2, 3];
+        const initSum = this.initValues.reduce((a, b) => a + b, 0);
+        this.state = {
+            sum: initSum
+        };
+        this.onUpdate = this.onUpdate.bind(this)
+    }
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{margin: 10, fontSize: 20, color: 'black'}}>总计 {this.state.sum}</Text>
+                <Counter style={{margin: 10}} onUpdate={this.onUpdate} initValue={this.initValues[0]}/>
+                <Counter style={{margin: 10}} onUpdate={this.onUpdate} initValue={this.initValues[1]}/>
+                <Counter style={{margin: 10}} onUpdate={this.onUpdate} initValue={this.initValues[2]}/>
+            </View>
+        );
+    }
+
+    onUpdate(oldValue, newValue) {
+        const valueChange = newValue - oldValue;
+        this.setState({sum: this.state.sum + valueChange})
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    }
+
+});
---
+++ src/component/Counter.js
@@ -0,0 +1,135 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    TextInput
+} from 'react-native';
+
+import PropTypes from 'prop-types';
+
+export default class Counter extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        //初始状态
+        this.state = {
+            value: this.props.initValue || 1
+        };
+        this._update = this._update.bind(this);
+    }
+
+    //默认属性
+    static defaultProps = {
+        initValue: 1,
+        onUpdate: f => f// 默认是一个什么都不做的函数。
+    };
+
+    render() {
+        return (
+            <View style={[this.props.style, styles.operatingBox]}>
+                <TouchableOpacity activeOpacity={0.2} onPress={this._reduce.bind(this)}>
+                    <View style={style.reduce}>
+                        <Text allowFontScaling={false} style={[styles.btn1]}>-</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <View style={style.inpBox}>
+                    <TextInput style={styles.inp1}
+                               returnKeyType='done'
+                               maxLength={3}
+                               onEndEditing={this._checkNum.bind(this)}
+                               value={this.state.value.toString()}
+                               keyboardType="numeric"
+                               onChangeText={(txt) => this._update(Number(txt))}
+                               autoFocus={false}
+                               underlineColorAndroid="transparent">
+
+                    </TextInput>
+                </View>
+                <TouchableOpacity activeOpacity={0.2} onPress={this._plus.bind(this)}>
+                    <View style={style.reduce}>
+                        <Text allowFontScaling={false} style={[styles.btn1]}>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>);
+    }
+
+    _checkNum() {
+        let value = this.state.value;
+        console.log(value);
+        if (value === '' || value < 1) {
+            value = 1;
+        } else {
+            value = Math.floor(value);
+        }
+        this._update(value)
+    }
+
+    _reduce() {
+        let value = this.state.value - 1;
+        if (value < 1) value = 1;
+        this._update(value);
+    }
+
+    _plus() {
+        this._update(this.state.value + 1)
+    }
+
+    _update(value) {
+        this.props.onUpdate(this.state.value, value)
+        this.setState({value: value})
+    }
+
+}
+//样式文件
+const styles = StyleSheet.create({
+    operatingBox: {
+        width: 120,
+        height: 35,
+        borderColor: '#ddd', // 边框颜色
+        borderWidth: 1,
+        flexDirection: 'row',//横向布局
+        alignItems: 'center',
+        borderRadius: 5, //圆角半径
+        overflow: 'hidden' // 超出控件范围的隐藏
+    },
+    btn1: {
+        fontSize: 18,
+        textAlign: 'center',
+        backgroundColor: 'transparent'
+    },
+    inpBox: {
+        flex: 1,
+        borderRightWidth: 1,
+        borderRightColor: '#ddd',
+    },
+    reduce: {
+        width: 34,
+        height: 34,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRightWidth: 1,
+        borderRightColor: '#ddd',
+    },
+    plus: {
+        width: 34,
+        height: 34,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inp1: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        textAlign: 'center',
+        padding: 0,
+        fontSize: 14
+    },
+})
+Counter.prototype = {
+    initValue: PropTypes.number,
+    style: PropTypes.object,
+    onUpdate: PropTypes.func
+}
